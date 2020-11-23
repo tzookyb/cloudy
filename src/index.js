800a6753168b248898fa1b26c5cf35e4a474c228
tzookyb <tzookyb@gmail.com>
2020-11-23 18:09:25 +0200

    fix sw

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App';
-import * as serviceWorker from './sw';
 import './assets/scss/global.scss';
 import reportWebVitals from './reportWebVitals';
-
-console.clear();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,4 +18,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorker.register();
